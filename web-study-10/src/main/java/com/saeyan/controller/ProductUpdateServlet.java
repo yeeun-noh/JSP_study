ec4e51a9e971f3ee898646b39f73f15797e4ceb6
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-06-15 00:47:39 +0900

    Update ProductUpdateServlet.java

--- web-study-10/src/main/java/com/saeyan/controller/ProductUpdateServlet.java
+++ web-study-10/src/main/java/com/saeyan/controller/ProductUpdateServlet.java
@@ -50,7 +50,7 @@ public class ProductUpdateServlet extends HttpServlet {
 		
 		System.out.println("pictureUrl :" + pictureUrl);
 		
-		//수정하지 않았을 떄 기본 사진 불러오기:
+		//수정하지 않았을 떄 기본 사진 불러오기 (nonmakeImg- hidden되어 있음):
 		if(pictureUrl == null) {
 			pictureUrl = multi.getParameter("nonmakeImg");
 		}
