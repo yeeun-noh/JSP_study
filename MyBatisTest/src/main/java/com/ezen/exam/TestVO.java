1dbee8b3951c200a6ac375b5578921a0425ab298
yeeun-noh <129915325+yeeun-noh@users.noreply.github.com>
2023-06-30 15:20:09 +0900

    Add files via upload

---
+++ MyBatisTest/src/main/java/com/ezen/exam/TestVO.java
@@ -0,0 +1,14 @@
+package com.ezen.exam;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TestVO {
+
+	int id;
+	String name, phone, address;
+}
---
+++ MyBatisTest/src/main/java/com/ezen/prj/MainClass.java
@@ -0,0 +1,27 @@
+package com.ezen.prj;
+
+import java.util.List;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		TestDAO tDao = TestDAO.getInstance();
+		
+//		List<TestVO> list = tDao.selectAllTest();		
+//		for(TestVO vo : list)
+//			System.out.println(vo);
+		
+		TestVO vo = new TestVO();
+		vo.setName("노예은");
+		vo.setPhone("010-1234-5678");
+		vo.setAddress("308호");
+		
+		int result = tDao.insertTest(vo);
+		
+		if(result == 1)
+			System.out.println("Insert 성공");
+		else 
+			System.out.println("Insert 실패");
+		
+	}
+}
